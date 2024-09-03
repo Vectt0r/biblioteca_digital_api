73a41094274b574b9730172d192c0caafdbd1dc3
Douglas Souza <119608044+Vectt0r@users.noreply.github.com>
2024-09-02 22:35:23 -0300

    Update biblioteca_digital_app.sql

--- biblioteca_digital_app.sql
+++ biblioteca_digital_app.sql
@@ -1,23 +1,17 @@
--- --------------------------------------------------------
--- Servidor:                     127.0.0.1
--- Versão do servidor:           8.0.30 - MySQL Community Server - GPL
--- OS do Servidor:               Win64
--- HeidiSQL Versão:              12.1.0.6537
--- --------------------------------------------------------
+-- Criação da tabela `books`
+CREATE TABLE IF NOT EXISTS books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_livro VARCHAR(255) NOT NULL,
+    nome_autor VARCHAR(255) NOT NULL,
+    genero VARCHAR(100),
+    editora VARCHAR(255),
+    numero_paginas INT
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
--- Exportação de dados foi desmarcado.
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+-- Inserção de dados de exemplo
+INSERT INTO books (nome_livro, nome_autor, genero, editora, numero_paginas) VALUES
+('O Senhor dos Anéis', 'J.R.R. Tolkien', 'Fantasia', 'HarperCollins', 1178),
+('1984', 'George Orwell', 'Distopia', 'Companhia das Letras', 352),
+('Dom Casmurro', 'Machado de Assis', 'Romance', 'Editora Globo', 224),
+('O Pequeno Príncipe', 'Antoine de Saint-Exupéry', 'Fábula', 'Editora Agir', 96),
+('A Revolução dos Bichos', 'George Orwell', 'Política', 'Companhia das Letras', 152);
